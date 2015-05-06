e7c8436c68a2c70aeaf040f565e4cc2dc1b270cf
SergioMyth <isleifurmj@gmail.com>
2015-05-06 15:24:15 +0000

    Search

---
+++ SozialHeap/SozialHeap/Scripts/search.js
@@ -0,0 +1,22 @@
+﻿<link rel="stylesheet" href="css/jquery-ui.css">
+<script src="js/jquery-ui.js"></script>
+<script>
+$(function() {
+    $("#tags").autocomplete({
+
+        source: "",
+        minLength: 1,
+        select: function(event, ui) {
+            var url = ui.item.id;
+            if(url != '#') {
+                location.href = '//' + url;
+            }
+        },
+        html: true, // optional (jquery.ui.autocomplete.html.js required)
+        // optional (if other layers overlap autocomplete list)
+        open: function(event, ui) {
+            $(".ui-autocomplete").css("z-index", 9990000);
+        }
+    });
+});
+</script>​
