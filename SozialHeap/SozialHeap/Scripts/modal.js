7c0a40068455d11ea2b47213b718e9980542d134
lommi <jonorn@gmail.com>
2015-05-12 11:33:57 +0000

    search

--- SozialHeap/SozialHeap/Scripts/modal.js
+++ SozialHeap/SozialHeap/Scripts/modal.js
@@ -95,6 +95,65 @@ $(function () {
 });
 
 $(function () {
+    if ($("#searchresult").length != 0) {
+
+        if (usersFound == 0)
+        {
+            $("#usersearchbutton").html('No users found');
+        }
+        else
+        {
+            $("#usersearchbutton").html('<input type="checkbox" checked /> Users (' + usersFound + ')');
+            $("#usersearchbutton, #usersearchbutton input").click(function () {
+                if ($(this).find('input').is(':checked'))
+                {
+                    $(this).find('input').prop('checked', false);
+                    $(".userfound").hide();
+                }
+                else
+                {
+                    $(this).find('input').prop('checked', true);
+                    $(".userfound").show();
+                }
+            });
+        }
+
+        if (groupsFound == 0) {
+            $("#groupsearchbutton").html('No groups found');
+        }
+        else {
+            $("#groupsearchbutton").html('<input type="checkbox" checked /> Groups (' + groupsFound + ')');
+            $("#groupsearchbutton, #groupsearchbutton input").click(function () {
+                if ($(this).find('input').is(':checked')) {
+                    $(this).find('input').prop('checked', false);
+                    $(".groupfound").hide();
+                }
+                else {
+                    $(this).find('input').prop('checked', true);
+                    $(".groupfound").show();
+                }
+            });
+        }
+
+        if (questionsFound == 0) {
+            $("#questionsearchbutton").html('No questions found');
+        }
+        else {
+            $("#questionsearchbutton").html('<input type="checkbox" checked /> Questions (' + questionsFound + ')');
+            $("#questionsearchbutton, #questionsearchbutton input").click(function () {
+                if ($(this).find('input').is(':checked')) {
+                    $(this).find('input').prop('checked', false);
+                    $(".questionfound").hide();
+                }
+                else {
+                    $(this).find('input').prop('checked', true);
+                    $(".questionfound").show();
+                }
+            });
+        }
+    }
+
+
     $('.postTR').click(function () {
         window.location = $(this).find('a').attr('href');
     }).hover(function () {
