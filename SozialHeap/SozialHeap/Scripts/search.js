d060b75581dc71fadcc3bd587f8712ffe0af1f21
Gummi St <gummi@climbing.is>
2015-05-14 14:39:54 +0000

    tiltekt í search.js

--- SozialHeap/SozialHeap/Scripts/search.js
+++ SozialHeap/SozialHeap/Scripts/search.js
@@ -1,21 +1,4 @@
-﻿/*
-var countries = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.whitespace,
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    // url points to a json file that contains an array of country names, see
-    // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
-    prefetch: '../data/countries.json'
-});
-
-// passing in `null` for the `options` arguments will result in the default
-// options being used
-$('#prefetch .typeahead').typeahead(null, {
-    name: 'countries',
-    source: countries
-});
-
-*/
-
+﻿
 $(function () {
 
     // tooltips in UserView
